1e9bfc45d550eb20a8810568786b2f7a04c94bfe
Maxwell Barvian <max@barvian.me>
2024-04-22 10:34:57 -0500

    Init

---
+++ astro.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel/serverless";
+
+import vue from "@astrojs/vue";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    isr: true
+  }),
+  integrations: [
+    vue({
+      script: {
+				propsDestructure: true
+			}
+    })
+  ]
+});
---
+++ src/components/VueComponent.vue
@@ -0,0 +1,6 @@
+<script setup>
+    const server = typeof window === 'undefined'
+</script>
+<template>
+    is server: {{ server }}
+</template>
