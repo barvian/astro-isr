237488958469e185ff80921141247e6465c1c861
Maxwell Barvian <max@barvian.me>
2024-04-23 07:51:21 -0500

    Add getimage

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
-
-import vue from "@astrojs/vue";
+import { defineConfig } from 'astro/config'
+import vercel from "@astrojs/vercel/serverless"
+import vue from "@astrojs/vue"
 
 // https://astro.build/config
 export default defineConfig({
